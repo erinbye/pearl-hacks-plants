372f674f09d0dcd6f69bcfddf1638f360e7fb220
gudetama <daniellewwise@gmail.com>
2021-02-20 18:04:38 -0500

    reformatted plant component to include destructured props

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,21 +1,30 @@
-import water from "../images/water-drop.jpg"
-import sun from "../images/sun.png"
-import fertilizer from "../images/fertilizer.png"
+import waterPic from "../images/water-drop.jpg";
+import sunPic from "../images/sun.png";
+import fertilizerPic from "../images/fertilizer.png";
 
-const Plant = ({plant}) => {
-    return (
-        <div>
-            <h1>{plant.name}</h1>
-            <h2>Level: {plant.level}</h2>
-            
-            <h3><img src={water} alt="water" width="50" height="50"/>: {plant.water}</h3>
-            <h3><img src={sun} alt="sun" width="50" height="50"/>: {plant.sunlight}</h3>
-            <h3><img src={fertilizer} alt="fertilizer" width="50" height="50"/>: {plant.fertilizer}</h3>
-            <img src={plant.picture}/>
-            <div/>
-            <button>Add {plant.name} to My Plants</button>
-        </div>
-    )
-}
+const Plant = ({ key, name, level, water, sunlight, fertilizer, picture }) => {
+  return (
+    // <div>
+    //     <h1>{plant.name}</h1>
+    //     <h2>Level: {plant.level}</h2>
 
-export default Plant
+    //     <h3><img src={water} alt="water" width="50" height="50"/>: {plant.water}</h3>
+    //     <h3><img src={sun} alt="sun" width="50" height="50"/>: {plant.sunlight}</h3>
+    //     <h3><img src={fertilizer} alt="fertilizer" width="50" height="50"/>: {plant.fertilizer}</h3>
+    //     <img src={plant.picture}/>
+    //     <div/>
+    //     <button>Add {plant.name} to My Plants</button>
+    // </div>
+    <li key={key}>
+      <img src={picture}></img>
+      <h3>{name}</h3>
+      <h3>{level}</h3>
+      <h3><img src={waterPic} alt="water" width="50" height="50" /> {water} </h3>
+      <h3><img src={sunPic} alt="sun" width="50" height="50" />{sunlight} </h3>
+      <h3><img src={fertilizerPic} alt="fertilizer" width="50" height="50" /> {fertilizer} </h3>
+      <button>Add {name} to My Plants</button>
+    </li>
+  );
+};
+
+export default Plant;
