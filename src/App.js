b1fcba46467381f2159299bd0e7c6a0e4f84ee5f
gudetama <daniellewwise@gmail.com>
2021-02-20 18:03:05 -0500

    removed plant child from app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import PlantList from "../src/components/PlantList"
 import {plants} from './data.json'
-import Plant from "./components/Plant"
 
 function App() {
   console.log(plants);
   return (
     <div className="App">
       <PlantList />
-      <Plant plant={plants[0]} />
     </div>
   );
 }
