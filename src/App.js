10e4b84dcf9e657105776607c8707d81f01ffb09
gudetama <daniellewwise@gmail.com>
2021-02-21 02:25:04 -0500

    commented out

--- src/App.js
+++ src/App.js
@@ -7,48 +7,48 @@ import MyPlants from './pages/MyPlants'
 import Explore from "./pages/Explore"
 import Plant from "./components/Plant"
 
-const plantDemo = [
-  {
-      id: 1,
-      name: "Aluminum Plant",
-      level: "Easy",
-      water: "once every week",
-      sunlight: "bright light",
-      fertilizer: "every two weeks",
-      picture: "https://www.guide-to-houseplants.com/images/aluminum-plant.jpg",
-      owned: false
-  },
-  {
-    id: 2,
-    name: "Baby Rubber Plant",
-    level: "Easy",
-    water: "once every week",
-    sunlight: "bright, indirect light",
-    fertilizer: "every two weeks",
-    picture: "https://www.guide-to-houseplants.com/images/peperomia-obtusifolia-window.jpg",
-    owned: false
-  },
-  {
-    id: 3,
-    name: "Caladium Plant",
-    level: "Medium",
-    water: "once every week",
-    sunlight: "bright, indirect light",
-    fertilizer: "every two weeks",
-    picture: "https://www.guide-to-houseplants.com/images/caladium-leaves.jpg",
-    owned: false
-  },
-  {
-    id: 4,
-    name: "Dragon Tree",
-    level: "Medium",
-    water: "once every week",
-    sunlight: "bright, indirect light",
-    fertilizer: "every two weeks",
-    picture: "https://www.guide-to-houseplants.com/images/dragon-tree.jpg",
-    owned: false
-  }
-]
+// const plants = [
+//   {
+//       id: 1,
+//       name: "Aluminum Plant",
+//       level: "Easy",
+//       water: "once every week",
+//       sunlight: "bright light",
+//       fertilizer: "every two weeks",
+//       picture: "https://www.guide-to-houseplants.com/images/aluminum-plant.jpg",
+//       owned: false
+//   },
+//   {
+//     id: 2,
+//     name: "Baby Rubber Plant",
+//     level: "Easy",
+//     water: "once every week",
+//     sunlight: "bright, indirect light",
+//     fertilizer: "every two weeks",
+//     picture: "https://www.guide-to-houseplants.com/images/peperomia-obtusifolia-window.jpg",
+//     owned: false
+//   },
+//   {
+//     id: 3,
+//     name: "Caladium Plant",
+//     level: "Medium",
+//     water: "once every week",
+//     sunlight: "bright, indirect light",
+//     fertilizer: "every two weeks",
+//     picture: "https://www.guide-to-houseplants.com/images/caladium-leaves.jpg",
+//     owned: false
+//   },
+//   {
+//     id: 4,
+//     name: "Dragon Tree",
+//     level: "Medium",
+//     water: "once every week",
+//     sunlight: "bright, indirect light",
+//     fertilizer: "every two weeks",
+//     picture: "https://www.guide-to-houseplants.com/images/dragon-tree.jpg",
+//     owned: false
+//   }
+// ]
 
 function App() {
   const [plantId, setPlantId] = useState(-1);
