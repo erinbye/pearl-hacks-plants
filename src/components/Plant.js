962de792caa7bb9cb0ef9331371232f52b135976
Erin <erinbye99@gmail.com>
2021-02-20 23:59:28 -0500

    my plants filtering

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,11 +1,15 @@
 import water from "../images/water-drop.jpg"
 import sun from "../images/sun.png"
 import fertilizer from "../images/fertilizer.png"
-import {plants} from "../data.json"
 
-const Plant = ({plantId}) => {
+const Plant = ({plantId, plants, setPage}) => {
     const plant = plants.filter((plant) => plant.id === plantId)[0];
-    console.log(plant);
+
+    const onAdd = () => {
+        plant.owned = !plant.owned;
+        setPage("My Plants");
+    }
+
     return (
         <div>
             <h1>{plant.name}</h1>
@@ -16,7 +20,8 @@ const Plant = ({plantId}) => {
             <h3><img src={fertilizer} alt="fertilizer" width="50" height="50"/>: {plant.fertilizer}</h3>
             <img src={plant.picture}/>
             <div/>
-            <button>Add {plant.name} to My Plants</button>
+            <button onClick={onAdd}>{plant.owned ? "Remove" : "Add"} {plant.name} {plant.owned ? "from" : "to"} My Plants</button>
+            
         </div>
     )
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -12,21 +12,42 @@ export const PlantList = ({plantData, page, setPlantId, setPage}) => {
   return (
     <div class="list">
       <h1>{page}</h1>
-      <ul>
-        {plantData.length ? (
-          plantData.map((plantData) => {
-            const {id, name, picture} = plantData
-            return (
-              <li key={id}>
-                <a onClick={() => onClickPlant(id)}><img src={picture}></img></a>
-                <h3>{name}</h3>
-              </li>
-            );
-        })
-        ) : (
-          <p>No Results to Display</p>
+      {page === "My Plants" ? (
+          <ul>
+          {plantData.length ? (
+            plantData.filter( (plant) => plant.owned === true )
+              .map((plant) => {
+              const {id, name, picture} = plant
+              return (
+                <div key={id}>
+                  <a onClick={() => onClickPlant(id)}><img src={picture}></img></a>
+                  <h3>{name}</h3>
+                </div>
+              );
+          })
+          ) : (
+            <p>No Results to Display</p>
           )}
-      </ul>
+        </ul>
+        ) : (
+          <ul>
+          {plantData.length ? (
+            plantData.map((plantData) => {
+              const {id, name, picture} = plantData
+              return (
+                <div key={id}>
+                  <a onClick={() => onClickPlant(id)}><img src={picture}></img></a>
+                  <h3>{name}</h3>
+                </div>
+              );
+          })
+          ) : (
+            <p>No Results to Display</p>
+          )}
+        </ul>
+        )
+      }
+      
   </div>
   )
 };
