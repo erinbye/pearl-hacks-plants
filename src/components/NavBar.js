24cc19e26ee7d789ce82dfc23b3cb49f16fae8ef
Erin <erinbye99@gmail.com>
2021-02-21 01:32:00 -0500

    gigi's main page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,6 @@
+import Logo from "../images/logo.svg"
+import "../pages/Main.css"
+
 const NavBar = ({setPage}) => {
 
     const onClick = (page) => {
@@ -6,6 +9,8 @@ const NavBar = ({setPage}) => {
 
     return (
         <div>
+            <h1><img className="disp" src={Logo} width="100" height="100" alt="Home Page Image"/></h1>
+            <h2 className="title">Bud Buddy</h2>
             <button onClick={() => onClick("Home")}>Home</button>
             <button onClick={() => onClick("My Plants")}>My Plants</button>
             <button onClick={() => onClick("Explore")}>Explore</button>
